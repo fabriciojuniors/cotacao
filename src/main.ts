ea089f1c9cf70ebb27be35bcc0de9b46480c32c6
Junior Souza <fabriciojuniorsc@gmail.com>
2021-02-28 19:30:20 -0300

    Ajustes tela de conversão

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,17 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+toggleDarkTheme(prefersDark.matches);
+
+// Listen for changes to the prefers-color-scheme media query
+prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+// Add or remove the "dark" class based on if the media query matches
+function toggleDarkTheme(shouldAdd) {
+  console.log("oi");
+  
+  document.body.classList.add('dark');
+}
