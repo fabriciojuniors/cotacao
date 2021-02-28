068b3d62b4f7833af2b908a1e6278e9678a4bff7
Junior Souza <fabriciojuniorsc@gmail.com>
2021-02-28 16:49:03 -0300

    Correção

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,7 +9,11 @@ import { MoedasServicesService } from '../service/moedas-services.service';
 })
 export class Tab2Page implements OnInit {
   moedas = [];
-
+   unmask(valor){
+    valor = valor.replace(/\D/g, "");
+    valor = valor.replace(/[\-]/,"");
+    return valor;
+}
   @ViewChild("moedaOriginal") moedaOriginal: IonSelect;
   @ViewChild("valorMoedaOriginal") valorMoedaOriginal: IonInput;
   @ViewChild("moedaDestino") moedaDestino: IonSelect;
@@ -27,7 +31,25 @@ export class Tab2Page implements OnInit {
       }
     )
   }
+  mask_valor(e){
+    let valor = e.target.value
+    valor = this.unmask(valor);
+    valor = valor.replace(/^[0]/,"");
+    valor = valor.replace(/(\d)?(\d{2}$)/, "$1,$2");
+    if(valor.length <= 3)
+        valor = "0"+valor;
+    if(valor.length > 6)
+        valor = valor.replace(/(\d{3})(\,\d{2})/g, ".$1$2");
 
+    if(valor.length >10)
+        valor = valor.replace(/(\d{3})(\.\d{3})(\,\d{2})/g, ".$1$2$3");
+
+    if(valor.length > 14)
+        valor = valor.replace(/(\d{3})(\.\d{3})(\.\d{3})(\,\d{2})/g, ".$1$2$3$4");    
+    
+    e.target.value = valor;
+    this.converter();
+}
   preencherMoedas(data) {
     for (const m in data) {
       let moeda = data[m];
