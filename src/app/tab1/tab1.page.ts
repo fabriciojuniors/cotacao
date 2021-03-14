c7a787e8c10ba014a786798ad43400abe3007548
Junior Souza <fabriciojuniorsc@gmail.com>
2021-03-14 12:39:36 -0300

    Alteração tela de listagem

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,75 +8,58 @@ import { MoedasServicesService } from '../service/moedas-services.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
-  moeda : {};
-  constructor(private moedas : MoedasServicesService,
-              private modalController : ModalController) {}
+export class Tab1Page implements OnInit {
+  moeda: {};
+  cotacao = [];
 
-  async ngOnInit(){
-     this.moedas.getAll()
+  slideOpts = {
+    initialSlide: 2,
+    speed: 400
+  };
+
+  constructor(private moedas: MoedasServicesService,
+    private modalController: ModalController) { }
+
+  async ngOnInit() {
+    this.moedas.getAll()
       .subscribe(
-        data =>{
+        data => {
           this.loadMoeda(data);
         },
-        error =>{
+        error => {
           console.log(error);
         }
       )
   }
 
-  loadMoeda(data){
+  loadMoeda(data) {
     this.moeda = data || {};
-    console.log(this.moeda);
-    
+
     let slider = document.querySelector("#slides");
-    let card = document.querySelector("#cards");3
-    let icon ="<ion-icon name='cash-outline' slot='start'></ion-icon>" ;
-    for(const m in this.moeda){
+    let card = document.querySelector("#cards"); 3
+    let icon = "<ion-icon name='cash-outline' slot='start'></ion-icon>";
+    for (const m in this.moeda) {
       let moeda = this.moeda[m];
-      
-      //Inicio - Gera slides
-      let item = document.createElement("ion-slide");
-      item.classList.add("container");
-      let div = document.createElement("div");
-      
-      let titulo = document.createElement("h1");
-      titulo.textContent = moeda.name;
+      this.cotacao.push({
+        moeda: moeda.code,
+        nome: moeda.name,
+        valorMaximo: moeda.high,
+        valorMinimo: moeda.low,
+        variacao: moeda.varBid
+      });
 
-      let img = document.createElement("img");
-      img.src = "../../assets/money.png";
-      img.style.height = "120px";
-      img.style.width = "120px";
+    }
 
-      let valor = document.createElement("h1");
-      valor.textContent = moeda.code + " " + Number(moeda.high).toFixed(2);
-      
-      div.appendChild(titulo);
-      div.appendChild(img);
-      div.appendChild(valor);
-
-      item.appendChild(div);
-      slider.appendChild(item);
-      //Fim - Gera Slides
-
-      //Inicio - Gera cards
-      let itemC = document.createElement("ion-item");
-      itemC.onclick = () => this.openModal(moeda.code);
-      itemC.innerHTML = icon + "<ion-label>"+moeda.name+"</ion-label>"+
-                        "<ion-badge color='success' slot='end'>"+moeda.code + " " + Number(moeda.high).toFixed(2)+"</ion-badge>";
-      card.appendChild(itemC);
-
-    } 
   }
 
-  alterarVisualizacao(evento){
+  alterarVisualizacao(evento) {
     document.querySelector("#slides").classList.toggle("hidden");
     document.querySelector("#cards").classList.toggle("hidden");
-    
+
   }
 
 
-  async openModal(moeda){
+  async openModal(moeda) {
     const modal = await this.modalController.create({
       component: MoedaDetalhesComponent,
       cssClass: 'my-custom-class',
